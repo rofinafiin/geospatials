56e6f036f838c3c43860b7f17aadd16ca6d6ed83
rofinafiin <rofinafiisr@gmail.com>
2024-02-02 00:27:29 +0000

    fix cuy

---
+++ geospatials-cloud.go
@@ -0,0 +1,268 @@
+package geospatials
+
+import (
+	"encoding/json"
+	pasproj "github.com/HRMonitorr/PasetoprojectBackend"
+	"github.com/rofinafiin/gcfbackend"
+	"net/http"
+	"os"
+)
+
+func GeoIntersectcloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(RequestGeoIntersects)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+		if !existing {
+			resp.Status = false
+			resp.Message = "Kamu kayaknya belum punya akun"
+		} else {
+			datageo, err := GeoIntersectQuery(conn, req.Coordinates)
+			if err != nil {
+				resp.Status = false
+				resp.Message = err.Error()
+			}
+			jsoncihuy, _ := json.Marshal(datageo)
+			resp.Status = true
+			resp.Message = "Data Berhasil diambil"
+			resp.Token = string(jsoncihuy)
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoWithincloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(RequestGeoIntersects)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoWithinQuery(conn, req.Coordinates)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoNearspherecloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(Nearspherereq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoNearSphereQuery(conn, req.Coordinates, req.Radius)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoBoxcloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(GeoBoxReq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoBoxQuery(conn, req.LowerLeft, req.LowerRight)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoCentercloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(Nearspherereq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoCenterQuery(conn, req.Coordinates, req.Radius)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoGeometrycloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(Geometryreq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoGeometryQuery(conn, req.Geometry)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoMaxDistancecloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(Nearspherereq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoMaxDistanceQuery(conn, req.Coordinates, req.Radius)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
+
+func GeoMinDistancecloud(publickey, Mongostring, dbname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	req := new(Nearspherereq)
+	conn := pasproj.MongoCreateConnection(Mongostring, dbname)
+	tokenlogin := r.Header.Get("Login")
+	if tokenlogin == "" {
+		resp.Status = false
+		resp.Message = "Header Login Not Exist"
+	} else {
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			resp.Message = "error parsing application/json: " + err.Error()
+		} else {
+			existing := gcfbackend.IsExist(tokenlogin, os.Getenv(publickey))
+			if !existing {
+				resp.Status = false
+				resp.Message = "Kamu kayaknya belum punya akun"
+			} else {
+				datageo, err := GeoMinDistanceQuery(conn, req.Coordinates, req.Radius)
+				if err != nil {
+					resp.Status = false
+					resp.Message = err.Error()
+				}
+				jsoncihuy, _ := json.Marshal(datageo)
+				resp.Status = true
+				resp.Message = "Data Berhasil diambil"
+				resp.Token = string(jsoncihuy)
+			}
+		}
+	}
+	return gcfbackend.ReturnStringStruct(resp)
+}
--- struct.go
+++ struct.go
@@ -1,5 +1,7 @@
 package geospatials
 
+import "go.mongodb.org/mongo-driver/bson"
+
 type GeoBorder struct {
 	Type        string        `bson:"type"`
 	Coordinates [][][]float64 `bson:"coordinates"`
@@ -12,4 +14,22 @@ type LocationData struct {
 	SubDistrict string    `bson:"sub_district"`
 	Village     string    `bson:"village"`
 	Border      GeoBorder `bson:"border"`
+}
+
+type RequestGeoIntersects struct {
+	Coordinates [][][]float64 `bson:"coordinates" json:"coordinates"`
+}
+
+type Nearspherereq struct {
+	Radius      int       `bson:"radius" json:"radius"`
+	Coordinates []float64 `bson:"coordinates" json:"coordinates"`
+}
+
+type GeoBoxReq struct {
+	LowerLeft  []float64 `json:"lowerLeft" bson:"lowerLeft"`
+	LowerRight []float64 `json:"lowerRight" bson:"lowerRight"`
+}
+
+type Geometryreq struct {
+	Geometry bson.M `json:"geometry" bson:"geometry"`
 }
